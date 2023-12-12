e0beea951cf6fdc5cba72e24773d8ddad67df888
spo0ds <asmitadhikari540@gmail.com>
2023-12-12 12:49:07 +0545

    script to retrieve the account info

---
+++ src/retrieve_account_info.rs
@@ -0,0 +1,10 @@
+#![deny(clippy::all)]
+use crate::get_account_info::{load_accounts, Account};
+
+pub fn retrieve_account_info(username: String, accounts_file: &str) -> Option<Account> {
+    if let Ok(accounts) = load_accounts(accounts_file) {
+        accounts.get(&username).cloned()
+    } else {
+        return None;
+    }
+}
